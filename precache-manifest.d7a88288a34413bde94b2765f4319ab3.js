540236e8287b983f08f77d4a5f4529cd3fe4fb57
Anshuman301 <anshumanprsd@gmail.com>
2020-05-23 21:52:36 +0530

    Updates

---
+++ precache-manifest.d7a88288a34413bde94b2765f4319ab3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071c3d0a9318235eeba08f185a258e23",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "78e3b69857c24f83836a",
+    "url": "/robo-friends/static/css/main.69bbcd74.chunk.css"
+  },
+  {
+    "revision": "6c67022f364b4d947554",
+    "url": "/robo-friends/static/js/2.607a90ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-friends/static/js/2.607a90ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e3b69857c24f83836a",
+    "url": "/robo-friends/static/js/main.7ec9062a.chunk.js"
+  },
+  {
+    "revision": "04531acf4b60b554d02d",
+    "url": "/robo-friends/static/js/runtime-main.7f2d94a8.js"
+  }
+]);
